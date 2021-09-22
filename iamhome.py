66a536a6fa921bce14d0b6c67caf8a526a1666de
yan-vei <yana.veitsman@yandex.kz>
2021-09-22 23:55:54 +0300

    debugging

--- iamhome.py
+++ iamhome.py
@@ -11,11 +11,3 @@ def handler(event, context):
     current_scene_id = event.get('state', {}).get(STATE_REQUEST_KEY, {}).get('scene')
     if current_scene_id is None:
         return DEFAULT_SCENE().reply(request)
-    current_scene = SCENES.get(current_scene_id, DEFAULT_SCENE)()
-    next_scene = current_scene.move(request)
-    if next_scene is not None:
-        print(f'Moving from scene {current_scene.id()} to {next_scene.id()}')
-        return next_scene.reply(request)
-    else:
-        print(f'Failed to parse user request at scene {current_scene.id()}')
-        return current_scene.fallback(request)
