0dd128882bb74992878e66833e2d7b48ad1c0432
yan-vei <yana.veitsman@yandex.kz>
2021-09-22 23:52:35 +0300

    feat(skill): adding scenes

--- iamhome.py
+++ iamhome.py
@@ -1,23 +1,21 @@
-# Globals
-GREETING = 'Здравствуйте! Я - помощник по проблемам с ЖКХ в вашем доме. \
-Хотите оформить заявку или проверить статус?'
+import json
+
+from state import STATE_REQUEST_KEY
+from scenes import SCENES, DEFAULT_SCENE
+from request import Request
+
 
 def handler(event, context):
-    if 'request' in event and \
-            'original_utterance' in event['request'] \
-            and len(event['request']['original_utterance']) > 0:
-        text = event['request']['original_utterance']
-    elif event['session']['new'] == True:
-        text = GREETING
-
-    return {
-        'version': event['version'],
-        'session': event['session'],
-        'response': {
-            # Respond with the original request or welcome the user if this \
-            # is the beginning of the dialog and the request has not yet been made.
-            'text': text,
-            # Don't finish the session after this response.
-            'end_session': 'false'
-        },
-    }
+    print('Incoming request: ' + json.dumps(event))
+    request = Request(event)
+    current_scene_id = event.get('state', {}).get(STATE_REQUEST_KEY, {}).get('scene')
+    if current_scene_id is None:
+        return DEFAULT_SCENE().reply(request)
+    current_scene = SCENES.get(current_scene_id, DEFAULT_SCENE)()
+    next_scene = current_scene.move(request)
+    if next_scene is not None:
+        print(f'Moving from scene {current_scene.id()} to {next_scene.id()}')
+        return next_scene.reply(request)
+    else:
+        print(f'Failed to parse user request at scene {current_scene.id()}')
+        return current_scene.fallback(request)
---
+++ scenes.py
@@ -0,0 +1,81 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+import inspect
+import sys
+
+from request import Request
+from state import STATE_RESPONSE_KEY
+
+class Scene(ABC):
+
+    @classmethod
+    def id(cls):
+        return cls.__name__
+
+    """Генерация ответа сцены"""
+    @abstractmethod
+    def reply(self, request):
+        raise NotImplementedError()
+
+    """Проверка перехода к новой сцене"""
+    def move(self, request: Request):
+        next_scene = self.handle_local_intents(request)
+        if next_scene is None:
+            next_scene = self.handle_global_intents(request)
+        return next_scene
+
+    @abstractmethod
+    def handle_global_intents(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def handle_local_intents(request: Request) -> Optional[str]:
+        raise NotImplementedError()
+
+    def fallback(self, request: Request):
+        return self.make_response('Извините, я вас не поняла. Пожалуйста, попробуйте повторить ваш ответ.')
+
+    def make_response(self, text, tts=None, card=None, state=None, buttons=None, directives=None):
+        response = {
+            'text': text,
+            'tts': tts if tts is not None else text,
+        }
+        if card is not None:
+            response['card'] = card
+        if buttons is not None:
+            response['buttons'] = buttons
+        if directives is not None:
+            response['directives'] = directives
+        webhook_response = {
+            'response': response,
+            'version': '1.0',
+            STATE_RESPONSE_KEY: {
+                'scene': self.id(),
+            },
+        }
+        if state is not None:
+            webhook_response[STATE_RESPONSE_KEY].update(state)
+        return webhook_response
+
+
+class Greeting():
+    def reply(self, request: Request):
+        text = ('Здравствуйте! Я - помощник по проблемам с ЖКХ в вашем доме. \
+Хотите оформить заявку или проверить статус?')
+        return self.make_response(text)
+
+
+def _list_scenes():
+    current_module = sys.modules[__name__]
+    scenes = []
+    for name, obj in inspect.getmembers(current_module):
+        if inspect.isclass(obj) and issubclass(obj, Scene):
+            scenes.append(obj)
+    return scenes
+
+
+SCENES = {
+    scene.id(): scene for scene in _list_scenes()
+}
+
+DEFAULT_SCENE = Greeting
