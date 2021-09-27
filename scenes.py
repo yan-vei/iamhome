9f5603d502d1d07cffa845c188f7b5250fab2511
yan-vei <yana.veitsman@yandex.kz>
2021-09-27 23:09:14 +0300

    fix(scenes): default reason arg

--- scenes.py
+++ scenes.py
@@ -154,7 +154,7 @@ class ApartmentInquiry(GenericInquiry):
 
 
 class FailedInquiry(ApartmentInquiry):
-    def __init__(self, reason):
+    def __init__(self, reason=None):
         self.reason = reason
 
     def reply(self, request: Request):
