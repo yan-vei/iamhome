fb8757a2cc37e637fb8a2db44fee2156d2bd3a15
yan-vei <yana.veitsman@yandex.kz>
2021-09-28 08:54:50 +0300

    test: test user problem field property

--- request.py
+++ request.py
@@ -20,7 +20,7 @@ class Request:
     @property
     def user_problem(self):
         state = self.request_body['state']
-        return(state.get['scene'])
+        return(state.get['session'].get('user_problem'))
         #if state.get('session').get('user_problem') is not None:
             #return state.get('session', {}).get('user_problem')
 
