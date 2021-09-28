227447c9a1cfb05fe821f202036b1788f80c4ae5
yan-vei <yana.veitsman@yandex.kz>
2021-09-28 08:50:32 +0300

    test: test user problem field property

--- request.py
+++ request.py
@@ -18,6 +18,13 @@ class Request:
             return state.get('application', {}).get('report_id')
 
     @property
+    def user_problem(self):
+        state = self.request_body['state']
+        return(state)
+        #if state.get('session').get('user_problem') is not None:
+            #return state.get('session', {}).get('user_problem')
+
+    @property
     def entities(self):
         return self.request_body['request'].get('nlu', {}).get('entities', {})
 
