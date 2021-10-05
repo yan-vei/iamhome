e0c79da0a302e66d597fef686ee1ee5c84929d0d
yan-vei <yana.veitsman@yandex.kz>
2021-10-05 09:06:32 +0300

    cleanup(intents) clean top intents

--- intents.py
+++ intents.py
@@ -94,7 +94,7 @@ HOUSE_INTENTS = \
          'category_id': 1,
          'subcategory_id': 1,
          'location': 3,
-        'informal_name': 'Холодно',
+        'informal_name': 'Нет отопления',
         'date_restriction': '15/10-12/05',
          'date_pronunciation': 'с 15 октября по 12 мая'
         },
@@ -238,7 +238,7 @@ HOUSE_INTENTS = \
          'category_id': 6,
          'subcategory_id': 1,
          'location': 3,
-        'informal_name': 'Изрисованы стены'
+        'informal_name': 'Надписи на стенах'
         },
         {'intent_name': 'paintHouse', # Неудовлетворительное состояние окраски фасада дома (кроме несанкционированных надписей/объявления на стенах дома)
          'category_id': 6,
