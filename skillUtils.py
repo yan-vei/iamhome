85a729e2baa033915b4389e7a1243b33806e4ae1
yan-vei <yana.veitsman@yandex.kz>
2021-10-05 08:49:01 +0300

    test(scenes): debug location

--- skillUtils.py
+++ skillUtils.py
@@ -1,4 +1,3 @@
-import intents
 import datetime
 
 def validate_address(street, house_number):
@@ -66,25 +65,3 @@ def getIntentLocation(intent_name):
     for intent in intents.APARTMENT_INTENTS:
         if intent_name == intent['intent_name']:
             return intent['location']
-
-
-def suggestTopIntents(location):
-    suggested_intents = []
-
-    if location == 'Location.APARTMENT':
-        top = intents.TOP_APARTMENT_INTENTS
-        print(top)
-        all_intents = intents.APARTMENT_INTENTS
-    elif location == 'Location.HOUSE':
-        top = intents.TOP_HOUSE_INTENTS
-        all_intents = intents.HOUSE_INTENTS
-
-    for top_intent in top:
-        for intent in all_intents:
-            if top_intent == intent['intent_name']:
-                suggested_intents.append(intent['informal_name'])
-                break
-
-    if suggested_intents != []:
-        return suggested_intents
-    return None
